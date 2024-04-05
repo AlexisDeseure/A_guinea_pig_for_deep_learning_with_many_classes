aaec63fa38a0d1e103b8cc2bbb3f75edbc416de4
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2024-04-05 23:29:31 +0200

    rename setup folders

--- setup_venv_bash.sh
+++ setup_venv_bash.sh
@@ -5,6 +5,4 @@ source ".venv/bin/activate"
 
 pip3 install -r requirements.txt
 
-deactivate
-
-echo "L'environnement virtuel a été créé avec succès."
+echo "L'environnement virtuel a ete cre avec succes."
--- setup_venv_powershell.bat
+++ setup_venv_powershell.bat
@@ -3,6 +3,4 @@ call .venv\Scripts\activate
 
 pip install -r requirements.txt
 
-desactivate
-
-echo "L'environnement virtuel a été créé avec succès."
+echo "L'environnement virtuel a ete cree avec succes."
